74d13cd6f7177110062eb5a6cd01148a50fa9720
Angel <angelgcdev@gmail.com>
2023-06-10 19:32:48 -0400

    link referenciados y tambien se coloco el feedback de los botones

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 const header__nav = document.querySelector(".header__nav");
 const BotonMenu = document.querySelector(".botonMenu");
+const botonesNav = document.querySelectorAll(".header__link");
+const homeButton = document.querySelector(".home__button");
+const aboutmeButton= document.querySelector('.aboutme__button');
 
+/*boton menu hamburguesa*/
 BotonMenu.addEventListener("click", () => {
-    console.log('click');
-    header__nav.classList.toggle("inactive");
+  header__nav.classList.toggle("inactive");
 });
 
-console.log('borrame');
+/*Botones de la barra de navegación*/
+botonesNav.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    header__nav.classList.toggle("inactive");
+  });
+});
+
+/*Boton de la seccion home */
+homeButton.addEventListener("click", () => {
+  const sectionPortfolio = document.querySelector("#portfolio");
+  sectionPortfolio.scrollIntoView({ behavior: "smooth" });
+});
+
+/*Boton de la seccion aboutme */
+aboutmeButton.addEventListener('click',()=>{
+  const secctionAboutme=document.querySelector('#contact');
+  secctionAboutme.scrollIntoView({behavior: 'smooth'});
+});
